2a016c8a040477fc19d173aecb469192881aa689
Shawn Murtagh <spmurtagh@gmail.com>
2016-12-15 23:33:51 -0500

    Add knockout to project and bundle config

--- Mybrary/App_Start/BundleConfig.cs
+++ Mybrary/App_Start/BundleConfig.cs
@@ -21,7 +21,8 @@ namespace Mybrary
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/knockout-3.4.0.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
